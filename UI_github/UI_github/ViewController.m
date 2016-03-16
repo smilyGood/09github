c9c0f0e987fb2c3daa70227a409b6a85cca19860
wyl <1052340662@qq.com>
2016-03-16 19:27:37 +0800

    asdas

--- UI_github/UI_github/ViewController.m
+++ UI_github/UI_github/ViewController.m
@@ -17,8 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"尼玛");
-    NSLog(@"我去");
+
 }
 
 - (void)didReceiveMemoryWarning {
